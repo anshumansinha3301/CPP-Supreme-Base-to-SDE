b6a109d91cca2324b2029246d3a43583e760457e
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-06-03 13:41:51 +0530

    Update Loop Code1.cpp

--- Loops/Loop Code1.cpp
+++ Loops/Loop Code1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    // Basic for loop to print numbers from 1 to 10
+    // Basic for loop to print numbers from 1 to 10 
     for (int i = 1; i <= 10; i++) {
         cout << i << " ";
     }
