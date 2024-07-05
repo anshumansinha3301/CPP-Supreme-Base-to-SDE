d27ec7d2ee2483c2a4a933e07f9d2f80d36d68ee
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-05 10:03:14 +0530

    Add files via upload

---
+++ Questions/Efficiency of a machine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double inputEnergy, outputEnergy, efficiency;
+    cout << "Enter input energy (in joules): ";
+    cin >> inputEnergy;
+    cout << "Enter output energy (in joules): ";
+    cin >> outputEnergy;
+    efficiency = (outputEnergy / inputEnergy) * 100;
+    cout << "Efficiency of the machine: " << efficiency << "%" << endl;
+    return 0;
+}
