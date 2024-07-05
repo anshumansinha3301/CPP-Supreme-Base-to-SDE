613751707cda235c66feec6b2f1bc2f394f0f58b
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-05 10:08:30 +0530

    Add files via upload

---
+++ Questions/Perimeter of Rhombus.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double side, perimeter;
+    cout << "Enter the length of a side of the rhombus: ";
+    cin >> side;
+    perimeter = 4 * side;
+    cout << "Perimeter of the rhombus: " << perimeter << " units" << endl;
+    return 0;
+}
