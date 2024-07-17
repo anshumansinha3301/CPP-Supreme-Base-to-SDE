6f2c54d7162fd574a37de2623013cfcaa1bfdb45
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-07-17 09:22:26 +0530

    Update Bollinger Bangs Indicator.cpp

--- Trading Patterns/Bollinger Bangs Indicator.cpp
+++ Trading Patterns/Bollinger Bangs Indicator.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-  
+// Bollinger Bangs function based code for integration into main code 
+// By SDE Anshuman Sinha
 std::pair<double, double> calculateBollingerBands(const std::vector<double>& prices, int period) {
     double sum = 0.0;
     for (int i = prices.size() - period; i < prices.size(); ++i) {
